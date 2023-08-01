830c77be15b37355d20ff7b42fdb5c16bf737927
Mariya Thomas <martho1172001@gmail.com>
2023-08-01 21:29:53 +0530

    assignment calculator

--- app.ts
+++ app.ts
@@ -1,15 +1,27 @@
-import express from "express";
+import { Calculator } from "./calculator";
+const myCalculator = new Calculator;
 
-const server = express();
+myCalculator.add(200,3);
+myCalculator.subtract(200,3);
+myCalculator.multiply(200,3);
+myCalculator.divide(200,3);
+myCalculator.power(21,3);
+myCalculator.factorial(12);
+myCalculator.modulus(200,3);
+myCalculator.percentage(20,3);
 
 
-server.get('/',(req,res)=>{
-    console.log("at /");
-    res.status(200).send("hello world typescript");
-    
-})
 
-server.listen(3000,()=>{
-    console.log("server is listening to port 3000");
-})
+
+// import express from "express";
+// const server = express();
+// server.get('/',(req,res)=>{
+//     console.log("at /");
+//     res.status(200).send("hello world typescript");   
+// })
+// server.listen(3000,()=>{
+//     console.log("server is listening to port 3000");
+// })
+
+
 
